0e73591f3c881fc65487a6ab7fe0f5eaf04f4e7c
NailyaNew <62836731+NailyaNew@users.noreply.github.com>
2020-06-02 16:48:44 +0300

    Add files via upload

---
+++ lab13/dist/lab13.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function viewBlock() {
+  const modWin = document.getElementById('modWin');
+  modWin.removeAttribute('hidden');
+}
+
+function hideBlock() {
+  const modWin = document.getElementById('modWin');
+  modWin.setAttribute('hidden', '');
+}
---
+++ lab13/js/lab13.js
@@ -0,0 +1,8 @@
+function viewBlock() {
+  const modWin = document.getElementById('modWin');
+  modWin.removeAttribute('hidden');
+}
+function hideBlock() {
+  const modWin = document.getElementById('modWin');
+  modWin.setAttribute('hidden', '');
+}
